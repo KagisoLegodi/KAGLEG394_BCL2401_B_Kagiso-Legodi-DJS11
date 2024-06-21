d21e0eba53d3594324caf77151a89c664c535600
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-06-21 11:06:39 +0200

    Rendered the App component wrapped with Redux Provider to make the Redux store available to all child components.

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,10 @@ import store from "./store";
 import { Provider } from "react-redux";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
