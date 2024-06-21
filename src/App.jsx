606731461aa970f4939ca211a6a7ab0d70038c35
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-06-21 02:01:17 +0200

    Add routing setup with react-router-dom for LoginPage, Profile, UploadPodcast, PodcastsPage, and PodcastDetailsPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import LoginPage from "./pages/LoginPage";
+import Profile from "./pages/Profile";
+import UploadPodcast from "./pages/UploadPodcast"
+import PodcastsPage from "./pages/Podcasts";
+import PodcastDetailsPage from "./pages/PodcastDetails";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/upload" element={<UploadPodcast />} />
+        <Route path="/podcasts" element={<PodcastsPage />} />
+        <Route path="/podcasts/:id" element={<PodcastDetailsPage />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
