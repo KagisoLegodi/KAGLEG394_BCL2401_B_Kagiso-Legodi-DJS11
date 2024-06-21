17066cebc34d70700ef9ee7d678b2ff6311b352a
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-06-21 11:02:27 +0200

    Rendered the main App component within a ThemeProvider for managing theme-related context.

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ThemeProvider } from './theme'; // Adjust the path based on your project structure
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
