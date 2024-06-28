a2c10635d97500fd2738abfdf8d9e4adb1ba58d3
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-06-28 02:38:01 +0200

    feat: Integrate SeasonDetails component and manage selected season state in PodcastDetailsPage
    - Fetch podcast data and set state for podcast and seasons.
    - Display podcast title, description, image, and list of seasons.
    - Use SeasonDetails component to manage and display episodes for the selected season.

--- src/pages/PodcastDetails.jsx
+++ src/pages/PodcastDetails.jsx
@@ -1,17 +1,18 @@
-/* eslint-disable react/jsx-no-undef */
-import { useEffect, useState } from "react";
-import Header from "../components/Utils/Header/index";
-import { useNavigate, useParams } from "react-router-dom";
-import { toast } from "react-toastify";
-import Button from "../components/Utils/Button/Button";
-import { auth } from "../firebase";
-import EpisodeDetails from "../components/Podcasts/EpisodeDetails";
+// eslint-disable react/jsx-no-undef
+import { useEffect, useState } from 'react';
+import Header from '../components/Utils/Header/index';
+import { useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import Button from '../components/Utils/Button/Button';
+import { auth } from '../firebase';
+import SeasonDetails from '../components/Podcasts/SeasonDetails';
 
 export default function PodcastDetailsPage() {
   const { id } = useParams();
   const navigate = useNavigate();
   const [podcast, setPodcast] = useState({});
   const [seasons, setSeasons] = useState([]);
+  const [selectedSeason, setSelectedSeason] = useState(null);
 
   useEffect(() => {
     if (id) {
@@ -21,13 +22,13 @@ export default function PodcastDetailsPage() {
           const data = await response.json();
 
           if (data) {
-            console.log("Fetched Podcast Data:", data);
+            console.log('Fetched Podcast Data:', data);
             setPodcast(data);
             setSeasons(data.seasons);
           } else {
-            console.log("No such Podcast!");
-            toast.error("No such Podcast!");
-            navigate("/podcasts");
+            console.log('No such Podcast!');
+            toast.error('No such Podcast!');
+            navigate('/podcasts');
           }
         } catch (e) {
           toast.error(e.message);
@@ -41,23 +42,23 @@ export default function PodcastDetailsPage() {
   return (
     <div>
       <Header />
-      <div className="input-wrapper" style={{ marginTop: "0rem" }}>
+      <div className="input-wrapper" style={{ marginTop: '0rem' }}>
         {podcast.id && (
           <>
             <div
               style={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                width: "100%",
-                margin: "1rem",
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                width: '100%',
+                margin: '1rem',
               }}
             >
               <h1 className="podcast-title-heading">{podcast.title}</h1>
               {podcast.createdBy === auth.currentUser?.uid && (
                 <Button
-                  width={"200px"}
-                  text={"Favourites"}
+                  width={'200px'}
+                  text={'Favourites'}
                   onClick={() => {
                     navigate(`/podcast/${id}/Favourites`);
                   }}
@@ -71,22 +72,13 @@ export default function PodcastDetailsPage() {
             <p className="podcast-description">{podcast.description}</p>
             <h1 className="podcast-title-heading">Seasons</h1>
             {seasons.length > 0 ? (
-              seasons.map((season, seasonIndex) => (
-                <div key={seasonIndex}>
-                  <h2>Season {seasonIndex + 1}</h2>
-                  {season.episodes.length > 0 ? (
-                    season.episodes.map((episode, episodeIndex) => (
-                      <EpisodeDetails
-                        key={episodeIndex}
-                        index={episodeIndex + 1}
-                        title={episode.title}
-                        description={episode.description}
-                      />
-                    ))
-                  ) : (
-                    <p>No Episodes</p>
-                  )}
-                </div>
+              seasons.map((season) => (
+                <SeasonDetails
+                  key={season.id}
+                  season={season}
+                  selectedSeason={selectedSeason}
+                  setSelectedSeason={setSelectedSeason}
+                />
               ))
             ) : (
               <p>No Seasons</p>
