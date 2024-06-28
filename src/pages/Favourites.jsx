8e4f9d822fc9e27460c5512c18440ab4a96fd25e
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-06-28 06:28:29 +0200

    created a simple Favourites page to build on later

---
+++ src/pages/Favourites.jsx
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const FavoritesPage = () => {
+  // Your logic for displaying favorite shows or podcasts goes here
+  return (
+    <div className="favorites-page">
+      <h1>Favorite Shows</h1>
+      <div className="favorites-list">
+        {/* Display your favorite shows here */}
+        {/* Example: */}
+        <div className="favorite-item">
+          <h2>Show Title</h2>
+          <p>Description of the show</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FavoritesPage;
