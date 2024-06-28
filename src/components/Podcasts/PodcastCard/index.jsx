e535f5f62a7e1339b285b6ed1f3598e3113838f9
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-06-28 05:20:29 +0200

    Refactor PodcastCard component to include PropTypes validation and render genres

--- src/components/Podcasts/PodcastCard/index.jsx
+++ src/components/Podcasts/PodcastCard/index.jsx
@@ -1,18 +1,42 @@
 import { Link } from "react-router-dom";
-
+import PropTypes from "prop-types";
 import "./styles.css";
-export default function PodcastsCard(item) {
-    // console.log(item.genre)
+
+const PodcastCard = ({ item }) => {
+  if (!item) {
+    return null;
+  }
+
+  const { id, title, displayImage, genres } = item;
+
   return (
-    <Link data={item} to={`/podcast/${item.id}`}>
+    <Link to={`/podcast/${id}`}>
       <div className="podcast-card">
-        <img
-          src={item.displayImage}
-          alt={item.title}
-          className="podcast-image"
-        />
-        <h4 className="podcast-title">{item.title}</h4>
+        <img src={displayImage} alt={title} className="podcast-image" />
+        <h4 className="podcast-title">{title}</h4>
+        <div className="genres-list">
+          {genres && genres.length > 0 ? (
+            genres.map((genre, index) => (
+              <span key={index} className="genre">
+                {genre}
+              </span>
+            ))
+          ) : (
+            <span className="genre">No genres specified</span>
+          )}
+        </div>
       </div>
     </Link>
   );
-}
+};
+
+PodcastCard.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    displayImage: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string),
+  }),
+};
+
+export default PodcastCard;
