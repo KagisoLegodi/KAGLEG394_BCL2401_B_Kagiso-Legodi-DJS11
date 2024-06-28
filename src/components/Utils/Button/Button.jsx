f01cfab7abeeff576c72501cf0e6780ddbf5efd6
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-06-28 05:47:37 +0200

    Refactor Button component to support Link for navigation
    Updated the Button component to conditionally render a Link component from React Router when a 'to' prop is provided, enabling navigation functionality.

--- src/components/Utils/Button/Button.jsx
+++ src/components/Utils/Button/Button.jsx
@@ -2,19 +2,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './styles.css';
+import { Link } from 'react-router-dom'; // Import Link from React Router
 
-function Button({ text, onClick, disabled }) {
-  return (
-    <div onClick={onClick} className="custom-btn" disabled={disabled}>
-      {text}
-    </div>
-  );
+function Button({ text, onClick, disabled, to }) {
+  if (to) {
+    // Render a Link component if `to` prop is provided
+    return (
+      <Link to={to} className="custom-btn" disabled={disabled}>
+        {text}
+      </Link>
+    );
+  } else {
+    // Render a regular button if `to` prop is not provided
+    return (
+      <button
+        type="button"
+        className="custom-btn"
+        onClick={onClick}
+        disabled={disabled}
+      >
+        {text}
+      </button>
+    );
+  }
 }
 
 Button.propTypes = {
   text: PropTypes.string.isRequired, // Requires `text` prop to be a string
   onClick: PropTypes.func, // `onClick` prop should be a function
   disabled: PropTypes.bool, // `disabled` prop should be a boolean
+  to: PropTypes.string, // Optional `to` prop for Link component
 };
 
 export default Button;
