fc4dcf7ed66350a93db8ba2bdd151ce8ff2f0c66
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-07-10 18:33:58 +0200

    Add season image to EpisodeDetails component

--- src/components/Podcasts/EpisodeDetails.jsx
+++ src/components/Podcasts/EpisodeDetails.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
 
-function EpisodeDetails({ index, title, description, audioFile }) {
+function EpisodeDetails({ index, title, description, audioFile, seasonImage }) {
   return (
     <div className="episode-details">
+      <img src={seasonImage} alt={`Season image for ${title}`} className="season-image" />
       <h2>Episode {index}: {title}</h2>
       <p>{description}</p>
       <audio controls className="custom-audio-player">
@@ -18,6 +19,7 @@ EpisodeDetails.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   audioFile: PropTypes.string.isRequired,
+  seasonImage: PropTypes.string.isRequired, // Add this line
 };
 
 export default EpisodeDetails;
