4df8d7f038712ecced2b4c4b03bd9dc3ecbf9d99
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-07-10 18:36:22 +0200

    Add season image to EpisodeDetails and apply styling for SeasonDetails component

--- src/components/Podcasts/SeasonDetails.jsx
+++ src/components/Podcasts/SeasonDetails.jsx
@@ -1,18 +1,21 @@
 import PropTypes from 'prop-types';
 import EpisodeDetails from './EpisodeDetails';
+import './SeasonDetails.css'; // Import the CSS file
 
 export default function SeasonDetails({ season }) {
   return (
-    <div>
+    <div className="season-details">
       {season.episodes.length > 0 ? (
         season.episodes.map((episode, episodeIndex) => (
-          <EpisodeDetails
-            key={episodeIndex}
-            index={episodeIndex + 1}
-            title={episode.title}
-            description={episode.description}
-            audioFile={episode.audioFile || "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"}
-          />
+          <div className="episode-card" key={episodeIndex}>
+            <EpisodeDetails
+              index={episodeIndex + 1}
+              title={episode.title}
+              description={episode.description}
+              audioFile={episode.audioFile || "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"}
+              seasonImage={season.displayImage} // Add this line
+            />
+          </div>
         ))
       ) : (
         <p>No Episodes</p>
@@ -25,6 +28,7 @@ SeasonDetails.propTypes = {
   season: PropTypes.shape({
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
+    displayImage: PropTypes.string.isRequired, // Add this line
     episodes: PropTypes.arrayOf(
       PropTypes.shape({
         title: PropTypes.string.isRequired,
