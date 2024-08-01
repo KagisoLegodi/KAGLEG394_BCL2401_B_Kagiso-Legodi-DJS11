df3512ec1738d46bca098961fce9be378b6cf526
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-08-01 09:43:50 +0200

    Remove favorite seasons and genres from FavoritesPage component.

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -24,20 +24,6 @@ const FavoritesPage = () => {
           <li key={index}>{episode.title}</li>
         ))}
       </ul>
-
-      <h1>Favorite Seasons</h1>
-      <ul>
-        {favoriteSeasons.map((season, index) => (
-          <li key={index}>{season.title}</li>
-        ))}
-      </ul>
-
-      <h1>Favorite Genres</h1>
-      <ul>
-        {favoriteGenres.map((genre, index) => (
-          <li key={index}>{genre}</li>
-        ))}
-      </ul>
     </div>
   );
 };
